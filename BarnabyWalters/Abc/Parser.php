a2ab8d90cb66433b3f31309f59faffb6439d3184
Barnaby Walters <barnaby@waterpigs.co.uk>
2013-06-06 21:15:21 +0000

    Converted PHP files from spaces to tabls

--- BarnabyWalters/Abc/Parser.php
+++ BarnabyWalters/Abc/Parser.php
@@ -14,69 +14,69 @@ namespace BarnabyWalters\Abc;
  * @author Barnaby Walters
  */
 class Parser {
-    public static function getHeaders($abc) {
-        // Split into lines
-        $lines = explode("\n", $abc);
-        
-        // Iterate through, parsing each header as we get to it
-        $headers = [];
-        
-        foreach ($lines as $l) {
-            $line = trim($l);
-            
-            // Ignore blank lines
-            if (empty($line))
-                continue;
-            
-            // Ignore non-header lines
-            if ($line[1] !== ':')
-                continue;
-            
-            // Split the line into header and value
-            $header = explode(':', $line, 2);
-            
-            // Add the info to the return array
-            $headers[$header[0]][] = trim($header[1]);
-        }
-        
-        return $headers;
-    }
-    
-    public static function getTuneBody($abc) {
-        // Is there a K: field? If not we can’t tell so return the whole thing
-        if (strstr($abc, 'K:') === false) {
-            return $abc;
-        } else {
-            // If there is return every line after K:
-            $lines = preg_split('/$\R?^/m', $abc);
-            
-            foreach ($lines as $i => $line) {
-                if ($line[1] == ':' and $line[0] !== '|')
-                    continue;
-                
-                // This one is the first body line, return lines after this joined by \n
-                return implode(PHP_EOL, array_slice($lines, $i));
-            }
-        }
-    }
-    
-    public static function getTuneHead($abc) {
-        // Is there a K: field? If not we can’t tell so return the whole thing
-        if (strstr($abc, 'K:') === false) {
-            return $abc;
-        } else {
-            // If there is return every line before K:
-            $lines = preg_split('/$\R?^/m', $abc);
-            
-            foreach ($lines as $i => $line) {
-                if ($line[1] == ':' and $line[0] !== '|')
-                    continue;
-                
-                // This one is the first non-header line, return < this joined by \n
-                return implode(PHP_EOL, array_slice($lines, 0, $i));
-            }
-        }
-    }
+	public static function getHeaders($abc) {
+		// Split into lines
+		$lines = explode("\n", $abc);
+		
+		// Iterate through, parsing each header as we get to it
+		$headers = [];
+		
+		foreach ($lines as $l) {
+			$line = trim($l);
+			
+			// Ignore blank lines
+			if (empty($line))
+				continue;
+			
+			// Ignore non-header lines
+			if ($line[1] !== ':')
+				continue;
+			
+			// Split the line into header and value
+			$header = explode(':', $line, 2);
+			
+			// Add the info to the return array
+			$headers[$header[0]][] = trim($header[1]);
+		}
+		
+		return $headers;
+	}
+	
+	public static function getTuneBody($abc) {
+		// Is there a K: field? If not we can’t tell so return the whole thing
+		if (strstr($abc, 'K:') === false) {
+			return $abc;
+		} else {
+			// If there is return every line after K:
+			$lines = preg_split('/$\R?^/m', $abc);
+			
+			foreach ($lines as $i => $line) {
+				if ($line[1] == ':' and $line[0] !== '|')
+					continue;
+				
+				// This one is the first body line, return lines after this joined by \n
+				return implode(PHP_EOL, array_slice($lines, $i));
+			}
+		}
+	}
+	
+	public static function getTuneHead($abc) {
+		// Is there a K: field? If not we can’t tell so return the whole thing
+		if (strstr($abc, 'K:') === false) {
+			return $abc;
+		} else {
+			// If there is return every line before K:
+			$lines = preg_split('/$\R?^/m', $abc);
+			
+			foreach ($lines as $i => $line) {
+				if ($line[1] == ':' and $line[0] !== '|')
+					continue;
+				
+				// This one is the first non-header line, return < this joined by \n
+				return implode(PHP_EOL, array_slice($lines, 0, $i));
+			}
+		}
+	}
 }
 
 // EOF
--- BarnabyWalters/Abc/Test/ParserTest.php
+++ BarnabyWalters/Abc/Test/ParserTest.php
@@ -12,107 +12,107 @@ require __DIR__ . '/../Parser.php';
  * @author Barnaby Walters
  */
 class ParserTest extends \PHPUnit_Framework_TestCase {
-    /**
-     * @dataProvider parseTests
-     * @param array $expected
-     * @param string $abc
-     */
-    public function testHeaderParsing(array $expected, $abc) {
-        $result = Parser::getHeaders($abc);
-        
-        $this->assertEquals($result, $expected);
-    }
-    
-    public function parseTests() {
-        return [
-            // Test Parses Single Header
-            [
-                ['K' => ['G']],
-                <<<EOT
+	/**
+	 * @dataProvider parseTests
+	 * @param array $expected
+	 * @param string $abc
+	 */
+	public function testHeaderParsing(array $expected, $abc) {
+		$result = Parser::getHeaders($abc);
+		
+		$this->assertEquals($result, $expected);
+	}
+	
+	public function parseTests() {
+		return [
+			// Test Parses Single Header
+			[
+				['K' => ['G']],
+				<<<EOT
 K:G
 abc|def
 EOT
-            ],
-            // Test Ignores Blank Lines
-            [
-                [
-                    'T' => ['Title'],
-                    'K' => ['A']
-                ],
-                <<<EOT
+			],
+			// Test Ignores Blank Lines
+			[
+				[
+					'T' => ['Title'],
+					'K' => ['A']
+				],
+				<<<EOT
 T:Title
 
 K:A
 EOT
-            ],
-            // Test Handles Headers within a tune body
-            [
-                [
-                    'T' => ['Title'],
-                    'K' => ['G']
-                ],
-                <<<EOT
+			],
+			// Test Handles Headers within a tune body
+			[
+				[
+					'T' => ['Title'],
+					'K' => ['G']
+				],
+				<<<EOT
 T:Title
 abc|def
 K:G
 EOT
-            ],
-            // Test Trims whitespace from values
-            [
-                [
-                    'T' => ['Title']
-                ],
-                <<<EOT
-T:  Title        
+			],
+			// Test Trims whitespace from values
+			[
+				[
+					'T' => ['Title']
+				],
+				<<<EOT
+T:	Title		 
 EOT
-            ],
-            // Test handles multiple header values
-            [
-                [
-                    'T' => ['Title'],
-                    'K' => ['C', 'G']
-                ],
-                <<<EOT
+			],
+			// Test handles multiple header values
+			[
+				[
+					'T' => ['Title'],
+					'K' => ['C', 'G']
+				],
+				<<<EOT
 T:Title
 K:C
 abc|def|gab
 K:G
 GAB|cde
 EOT
-            ]
-        ];
-    }
-    
-    public function testGetTuneBodyReturnsWholeStringIfNoKSet() {
-        $test = <<<EOT
+			]
+		];
+	}
+	
+	public function testGetTuneBodyReturnsWholeStringIfNoKSet() {
+		$test = <<<EOT
 some random multiline
 text and stuff
 EOT;
-        $result = Parser::getTuneBody($test);
-        $this->assertEquals($result, $test);
-    }
-    
-    public function testGetTuneBodyReturnsLinesAfterK() {
-        $test = <<<EOT
+		$result = Parser::getTuneBody($test);
+		$this->assertEquals($result, $test);
+	}
+	
+	public function testGetTuneBodyReturnsLinesAfterK() {
+		$test = <<<EOT
 T:The Title
 K:G
 this is the text
 which should be returned
 EOT;
-        $result = Parser::getTuneBody($test);
-        $this->assertEquals("this is the text\nwhich should be returned", $result);
-    }
-    
-    public function testGetTuneHeadReturnsLinesBeforeK() {
-        $test = <<<EOT
+		$result = Parser::getTuneBody($test);
+		$this->assertEquals("this is the text\nwhich should be returned", $result);
+	}
+	
+	public function testGetTuneHeadReturnsLinesBeforeK() {
+		$test = <<<EOT
 T:The Title
 K:G
 this text
 which should not be returned
 EOT;
-        $result = Parser::getTuneHead($test);
-        $this->assertEquals("T:The Title\nK:G", $result);
-    }
+		$result = Parser::getTuneHead($test);
+		$this->assertEquals("T:The Title\nK:G", $result);
+	}
 }
 
 // EOF
